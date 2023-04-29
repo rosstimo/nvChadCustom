4dddb1928dd9318a1e6d1331c7b19f922181bb4d
rosstimo <rosstimo@isu.edu>
2023-04-29 05:42:20 -0600

    update settings

--- chadrc.lua
+++ chadrc.lua
@@ -11,6 +11,63 @@ M.ui = {
   hl_override = highlights.override,
   hl_add = highlights.add,
   transparency = true,
+
+  -- cmp themeing
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+    border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
+    selected_item_bg = "colored", -- colored / simple
+  },
+
+  telescope = { style = "bordered" }, -- borderless / bordered
+
+  ------------------------------- nvchad_ui module
+  ------------------------------- nvchad_ui moduless -----------------------------
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "arrow",
+    overriden_modules = nil,
+  },
+
+  -- lazyload it when there are 1+ buffers
+  tabufline = {
+    show_numbers = false,
+    enabled = true,
+    lazyload = true,
+    overriden_modules = nil,
+  },
+
+  -- nvdash (dashboard)
+  nvdash = {
+    load_on_startup = false,
+
+    header = {
+      },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc b m", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+
+  cheatsheet = { theme = "grid" }, -- simple/grid
+
+  lsp = {
+    -- show function signatures i.e args as you type
+    signature = {
+      disabled = false,
+      silent = true, -- silences 'no signature help available' message from appearing
+    },
+  },
+
 }
 
 M.plugins = "custom.plugins"
