58bebdbc70cccf5af0554ab0b49b8c998b48e1aa
rosstimo <rosstimo@isu.edu>
2023-07-24 15:28:05 -0600

    minor fix

--- plugins.lua
+++ plugins.lua
@@ -28,25 +28,10 @@ local plugins = {
     opts = overrides.mason
   },
 
-  -- {
-    -- "nvim-treesitter/nvim-treesitter",
-    -- "p00f/nvim-ts-rainbow",
-    -- require("nvim-treesitter.configs").setup {
-    --   highlight = {
-    --     -- ...
-    --   },
-    --   -- ...
-    --   rainbow = {
-    --     enable = true,
-    --     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    --     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    --     max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    --     -- colors = {}, -- table of hex strings
-    --     -- termcolors = {} -- table of colour name strings
-    --   },
-    -- },
-    -- opts = overrides.treesitter,
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
 
   {
     "nvim-tree/nvim-tree.lua",
