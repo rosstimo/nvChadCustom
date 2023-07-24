9f295704c8c9d42a35e5d674f23dad8737b280b7
rosstimo <rosstimo@isu.edu>
2023-07-24 07:21:43 -0600

    update nvimtree conf

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -38,18 +38,68 @@ M.mason = {
 
 -- git support in nvimtree
 M.nvimtree = {
+  hijack_unnamed_buffer_when_opening = true,
+
+  update_focused_file = {
+    enable = true,
+    update_root = true,
+  },
+  view = {
+    adaptive_size = true,
+    side = "left",
+    width = 10,
+    preserve_window_proportions = true,
+  },
   git = {
     enable = true,
   },
 
   renderer = {
+    root_folder_label = true,
     highlight_git = true,
+    highlight_opened_files = "none",
+
+    indent_markers = {
+      enable = false,
+    },
+
     icons = {
       show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
         git = true,
+      },
+
+      glyphs = {
+        default = "~",
+        symlink = "",
+        folder = {
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+          symlink_open = "",
+          arrow_open = "",
+          arrow_closed = "",
+        },
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌",
+        },
       },
     },
   },
+  -- view = {
+  --   width = 30,
+  -- },
+
 }
 
 M.cmp = {
